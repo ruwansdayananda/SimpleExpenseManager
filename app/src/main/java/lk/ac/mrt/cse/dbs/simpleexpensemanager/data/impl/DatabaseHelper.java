fdf068e89872064243c654f2c36399cdf5574e82
ruwansdayananda <ruwansampathdayananda@gmail.com>
2021-02-02 01:51:56 +0530

    Changed the database file name from ExpenseManager.db to 180109R.db

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/DatabaseHelper.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/DatabaseHelper.java
@@ -8,7 +8,7 @@ import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;           //version of current database
-    public static final String DATABASE_NAME = "ExpenseManager.db";    //Database name
+    public static final String DATABASE_NAME = "180109R.db";    //Database name
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
